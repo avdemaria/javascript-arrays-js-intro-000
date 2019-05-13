3a004084e9aba1ebdc336ea4e1c0dc13aa73b42b
trustworthy-request-7186 <avdemaria@gmail.com>
2019-05-13 16:20:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,11 @@ function addElementToBeginningOfArray(chocolateBars, candyString){
 function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
   return chocolateBars.unshift(candyString);
 }
+
+function addElementToEndOfArray(chocolateBars, candyString){
+  return [...chocolateBars, candyString];
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
+  return chocolateBars.push(candyString);
+}
