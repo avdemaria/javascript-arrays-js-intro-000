a955c2f58f9f24aee79aef8cc8c48ae6a34cc0c9
trustworthy-request-7186 <avdemaria@gmail.com>
2019-05-13 16:16:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array, element){
-  return array = (element, ...array);
+function addElementToBeginningOfArray(chcolateBars, candyString){
+  return [candyString, ...chcoclateBars];
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element){
-  return array.unshift(element);
+function destructivelyAddElementToBeginningOfArray(chcoclateBars, candyString){
+  return array.unshift(candyString);
 }
